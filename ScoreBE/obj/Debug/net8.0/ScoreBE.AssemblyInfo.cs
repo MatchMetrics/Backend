c33576025d29b298d24115d9f652e4a577913249
prsoderlund <prsoderlund@gmail.com>
2024-10-17 21:37:07 +0200

    Fix package reference openApi

--- ScoreBE/obj/Debug/net8.0/ScoreBE.AssemblyInfo.cs
+++ ScoreBE/obj/Debug/net8.0/ScoreBE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScoreBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece5d15974adf142afbff9b1aa4d3b52e58a3619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bda2a3bd7edb739bdc949bd3925602f182e8387")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScoreBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScoreBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
